93d17008208bfe2fec3427c7e6674dc7be204178
Arkadiy Shuvaev <Arkadiy_Shuvaev@epam.com>
2018-04-19 17:51:28 +0300

    Creating a Custom Action Method Selector

---
+++ Paladin/Paladin/Controllers/PromotionsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Paladin.Infrastructure;
+
+namespace Paladin.Controllers
+{
+	public class PromotionsController : Controller
+    {
+	    [IsPaladinMobile]
+		public ActionResult Promos()
+		{
+			return View();
+		}
+
+	    [IsPaladinMobile]
+		public ActionResult GoPromo()
+        {
+            return View();
+        }
+    }
+}
---
+++ Paladin/Paladin/Infrastructure/IsPaladinMobile.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+
+namespace Paladin.Infrastructure
+{
+	// Let's imagine that we have mobile application that send us the x-PaladinMobile:true header
+	public class IsPaladinMobile : ActionMethodSelectorAttribute
+	{
+		public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+		{
+			return controllerContext.HttpContext.Request.Headers["x-PaladinMobile"] != null;
+		}
+	}
+}
